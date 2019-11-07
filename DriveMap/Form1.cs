b9da2c24df46f40f987306d84a152b91bd3503e3
CarloCapuz <36867035+CarloCapuz@users.noreply.github.com>
2019-11-07 11:48:51 -0500

    Add files via upload

---
+++ DriveMap/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DriveMap
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        } // end Form1()
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // textbox input
+            string userinput = textBox1.Text;
+
+            // letter variable - drive letter
+            string letter = "P";
+
+            var process = new Process();
+            var startInfo = new ProcessStartInfo
+            {
+                WindowStyle = ProcessWindowStyle.Hidden,
+                FileName = "net.exe",
+                Arguments = "USE " + letter + ": /DELETE"
+            };
+            process.StartInfo = startInfo;
+            process.Start();
+            process.WaitForExit();
+
+            // ****
+            string path = @"\\conc.students.local\student";
+
+            var pro = new Process();
+            var psi = new ProcessStartInfo
+            {
+                //WindowStyle = ProcessWindowStyle.Hidden,
+                FileName = "net.exe",
+                Arguments = "USE " + letter + ": " + path + @" /USER:CONC\" + userinput
+            };
+            pro.StartInfo = psi;
+            pro.Start();
+            pro.WaitForExit();
+
+        } // end button1_Click
+
+    } // end class
+} // end namespace
